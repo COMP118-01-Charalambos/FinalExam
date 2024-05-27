97db14d7e40fdb27b454f9536163b53f00eab00d
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-05-27 17:26:37 +0300

    created a unit test for function sumPositive. Am working from macbook

--- FinalExam/UnitTest.cpp
+++ FinalExam/UnitTest.cpp
@@ -1,8 +1,22 @@
-//
-//  UnitTest.cpp
-//  FinalExam
-//
-//  Created by Charalambos Hadjigeorgiou on 27/05/2024.
-//
+/** \file UnitTest.cpp
+ * \brief a unit test for matrix program
+ * \date 27/5/2024
+ * \author Charalambos Hadjigerorgiou
+ */
 
 #include <stdio.h>
+
+TEST_CLASS(FinalExam) {
+public:
+    TEST_METHOD(sumPositive) {
+        //Act
+        const int MAX_ROW = 2;
+        double mat[MAX_ROW][MAX_COL];
+        int choice, maxRow = 2;
+        
+        //Arrange
+        fillMatrix(mat, maxRow);
+        
+        //Assert
+        Assert::AreEqual(0, sumPositive(mat, maxRow));
+}
