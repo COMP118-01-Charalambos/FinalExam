273e24189cea32bc857abd1e17b154833d17bb1e
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-05-27 16:21:22 +0300

    Initial commit

--- FinalExam/main.cpp
+++ FinalExam/main.cpp
@@ -1,14 +1,42 @@
-//
-//  main.cpp
-//  FinalExam
-//
-//  Created by Charalambos Hadjigeorgiou on 27/05/2024.
-//
+/** \file FinalExam.cpp
+ * \brief A small matrix math program
+ * \date 27/5/24
+ * \author Charalambos Hadjigeorgiou
+ */
 
-#include <iostream>
+#include<iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main () {
+    const int MAX_COL = 5;
+    const int MAX_ROW = 2;
+    int mat[MAX_COL][MAX_ROW];
+    int choice;
+    
+    do {
+        cout << "\n1) Show data in matrix";
+        cout << "\n2) Enter data in matrix (range -0.5 to 4.5)";
+        cout << "\n3) Sum of all positive numbers (i.e. values > 0)";
+        cout << "\n4) Exit";
+        cout << "\nPlease enter choice: ";
+        cin >> choice;
+        
+        switch(choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    } while(choice != 4);
+        cerr << "Wrong Input! Please try again";
+        return 0;
 }
+
+    
+
+
